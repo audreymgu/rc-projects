3c0bf0bfb0f05e75a24c2752ce49bf4c40579de3
Audrey Gu <audreymgu@gmail.com>
2026-04-11 06:33:01 -0400

    Refactor LM as its own module.

--- fortune-bot/index.js
+++ fortune-bot/index.js
@@ -1,4 +1,7 @@
-const express = require("express");
+import { LineusManager } from "./modules/lineus-manager.js";
+import express from "express";
+import fs from "node:fs/promises";
+
 const app = express();
 const port = 3000;
 
@@ -12,18 +15,16 @@ app.listen(port, () => {
 
 // receive client commands
 app.post( ['/hello', '/input', '/reset'], (req, res) => {
-  lm.buffer(req.body.commands);
+  // lm.buffer(req.body.commands);
   // HTTP return status, just says "it worked"
   res.sendStatus(200);
 })
 
 app.post( ['/shape'], (req, res) => {
   console.log(JSON.stringify(req.body));
-  appendData(JSON.stringify(req.body));
+  appendData(JSON.stringify(req.body.data));
   res.sendStatus(200);
 })
-
-const fs = require('node:fs/promises');
 
 async function appendData(data) {
   try {
@@ -34,65 +35,13 @@ async function appendData(data) {
   }
 }
 
-const net = require("net");
-
-class LineusManager {
-  constructor() {
-    this.host = "10.100.2.219";
-    this.client = new net.Socket();
-    this.socketState = "";
-    this.commandBuffer = [];
-  }
-
-  init() {
-    // connect to line-us
-    this.client.connect(1337, this.host, function () {
-      console.log("Connected");
-    });
-
-    this.client.on("data", (data) => {
-      console.log("Received: " + data);
-
-      // poll for messages after successful connection
-      // or queue next command after previous success
-      if (data.indexOf("hello") == 0 || data.indexOf("ok") == 0) {
-        this.socketState = 'ready'
-        this.maybeSendMessage()
-      }
-
-      // disconnect on error
-      if (data.indexOf("error") == 0) {
-        console.log("Error in command " + cmdIndex);
-        console.log("Disconnecting...");
-        client.destroy()
-      }
-    });
-  }
-
-  maybeSendMessage() {
-    if(this.commandBuffer.length > 0 && this.socketState == 'ready') {
-        let message = this.commandBuffer.pop();
-        this.client.write(message + '\x00\n');
-    } else {
-        // what "this" refers to changes depending on the context, the arrow function binds to where function was called
-        setTimeout(() => {this.maybeSendMessage()}, 500);
-    }
-  }
-
-  buffer(commands) {
-    commands.push("G28");
-    commands.reverse();
-    this.commandBuffer.push(...commands);
-  }
-}
-
-// create instance
-const lm = new LineusManager();
+// create LM instance
+// const lm = new LineusManager();
 
 // pass command buffer
 // lm.buffer(commands)
 
 // run initialization
-lm.init();
+// lm.init();
 
 // setTimeout(() => {console.log('beep')}, 1000);
---
+++ fortune-bot/modules/lineus-manager.js
@@ -0,0 +1,52 @@
+// const net = require("net");
+import net from "net";
+
+export class LineusManager {
+  constructor() {
+    this.host = "10.100.2.219";
+    this.client = new net.Socket();
+    this.socketState = "";
+    this.commandBuffer = [];
+  }
+
+  init() {
+    // connect to line-us
+    this.client.connect(1337, this.host, function () {
+      console.log("Connected");
+    });
+
+    this.client.on("data", (data) => {
+      console.log("Received: " + data);
+
+      // poll for messages after successful connection
+      // or queue next command after previous success
+      if (data.indexOf("hello") == 0 || data.indexOf("ok") == 0) {
+        this.socketState = 'ready'
+        this.maybeSendMessage()
+      }
+
+      // disconnect on error
+      if (data.indexOf("error") == 0) {
+        console.log("Error in command " + cmdIndex);
+        console.log("Disconnecting...");
+        client.destroy()
+      }
+    });
+  }
+
+  maybeSendMessage() {
+    if(this.commandBuffer.length > 0 && this.socketState == 'ready') {
+        let message = this.commandBuffer.pop();
+        this.client.write(message + '\x00\n');
+    } else {
+        // what "this" refers to changes depending on the context, the arrow function binds to where function was called
+        setTimeout(() => {this.maybeSendMessage()}, 500);
+    }
+  }
+
+  buffer(commands) {
+    commands.push("G28");
+    commands.reverse();
+    this.commandBuffer.push(...commands);
+  }
+}
